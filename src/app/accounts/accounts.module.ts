2e30b70f1f50232b11604452c985ae81d0d23e84
Bhagavan Reddy <bhagavan44@gmail.com>
2021-06-06 14:20:37 +0530

    #24 Seperated accounts based on status

--- src/app/accounts/accounts.module.ts
+++ src/app/accounts/accounts.module.ts
@@ -6,10 +6,12 @@ import { AccountsRoutingModule } from './accounts.routing.module';
 import { IndexComponent } from './index/index.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SaveComponent } from './save/save.component';
+import { AccountListComponent } from './index/account-list/account-list.component';
 
 @NgModule({
   declarations: [
     IndexComponent,
+    AccountListComponent,
     SaveComponent
   ],
   imports: [
