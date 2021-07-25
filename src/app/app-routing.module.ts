3aa3a8319be624c6de279d311611c00620051def
Bhagavan Reddy <bhagavan44@gmail.com>
2021-07-25 16:19:29 +0530

    Bottom tabs added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,41 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
 import { AuthGuard } from './core/guards/auth.gaurd';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule) },
   {
-    path: 'dashboard',
-    component: DashboardComponent,
-    canLoad: [AuthGuard]
-  },
-  { path: 'about', component: AboutComponent, canLoad: [AuthGuard] },
-  {
-    path: 'accounts',
-    loadChildren: () => import('./accounts/accounts.module').then(m => m.AccountsModule),
-    canLoad: [AuthGuard]
-  },
-  {
-    path: 'settings',
-    loadChildren: () => import('./settings/settings.module').then(m => m.SettingsModule),
-    canLoad: [AuthGuard]
-  },
-  {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then(m => m.FolderPageModule),
-    canLoad: [AuthGuard]
-  },
-  {
-    path: 'entities',
-    loadChildren: () => import('./entities/entities.module').then(m => m.EntitiesModule),
-    canLoad: [AuthGuard]
-  },
-  {
-    path: 'transactions',
-    loadChildren: () => import('./transaction/transaction.module').then(m => m.TransactionModule),
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsModule),
     canLoad: [AuthGuard]
   }
 ];
