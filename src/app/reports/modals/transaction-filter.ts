ec83bdf4b8afaaeb49addae452a6f9c9ad2c7864
Bhagavan Reddy <bhagavan44@gmail.com>
2022-01-16 23:23:38 +0530

    Reports frame work and transaction history report

---
+++ src/app/reports/modals/transaction-filter.ts
@@ -0,0 +1,10 @@
+import { PageRequest } from 'src/app/models/page-request';
+
+export class TransactionFilter extends PageRequest {
+    accountIds?: number[];
+    transactionTypes?: string[];
+    fromAmount?: number;
+    toAmount?: number;
+    fromDate?: Date;
+    toDate?: Date;
+}
